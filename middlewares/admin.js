1dee63422bceafe02d2940c4042fb529aecdbd0e
arshedgithub <arshedahmedmail@gmail.com>
2024-08-29 22:38:41 +0530

    remove console logs

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    console.log(req.user);
-    // if (!req.user.role) return res.status(403).send("Access denied." );    
+    if (!req.user.isAdmin) return res.status(403).send("Access denied." );    
     next();
 }
