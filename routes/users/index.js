19c93fbf3f8eb048a90c653f52bb639ca04d2d35
arshedgithub <arshedahmedmail@gmail.com>
2024-09-01 08:51:34 +0530

    delete user details from response

--- routes/users/index.js
+++ routes/users/index.js
@@ -11,7 +11,7 @@ const router = express.Router();
 const User = db.User;
 
 router.get('/', auth, admin, async (req, res, next) => {
-    const users = await User.findAll({exclude: ['password']});
+    const users = await User.findAll({ exclude: ['password'] });
     res.status(200).json(users);
     next();
 });
@@ -40,17 +40,18 @@ router.post('/auth/signin', async (req, res, next) => {
     // (visit vidly backend)
     // use role from user table in jwt payload
 
-    const user = await User.findOne({ where: { username: username, password: password}});
+    const user = await User.findOne({ where: { username: username, password: password } });
 
     if (username == process.env.ADMIN_USERNAME && password == process.env.ADMIN_PASSWORD) {
-        const userPayload = { username: username, isAdmin: true, user: "admin" }
+        const userPayload = { username: username, isAdmin: true, user: "Admin" }
         const accessToken = jwt.sign(userPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3h", algorithm: "HS512" });
-        res.json({ accessToken });
+        res.status(200).json({ accessToken });
 
-    } else if (user){
+    } else if (user) {
         const userPayload = { username: username, isAdmin: false, user: user.id }
         const accessToken = jwt.sign(userPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "3h", algorithm: "HS512" });
-        res.json({ userPayload, accessToken });
+        res.status(200).json({ accessToken });
+
     } else {
         res.status(401).json({ message: "Invalid username or password." });
     }
@@ -62,7 +63,7 @@ router.post('/auth/signup', async (req, res, next) => {
         const { username, name, password, email, address, contact } = req.body;
         const new_user = { username, name, password, email, address, contact };
         const user = await User.create(new_user);
-        res.status(200).json({username: user.username, name: user.name, contact: user.contact});
+        res.status(200).json({ username: user.username, name: user.name, contact: user.contact });
         next();
     } catch (error) {
         res.status(400).json(error.message);
